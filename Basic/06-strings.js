830b6fb55459a15612f11d5aa868945489b4a3f1
esti-martin <estibalizmartinborja@gmail.com>
2025-04-22 08:37:39 +0200

    06 y 07 Strings

--- Basic/06-strings.js
+++ Basic/06-strings.js
@@ -5,44 +5,44 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=6565
 
 // Strings (cadenas de texto)
 
-// Concatenación
+// Concatenación de cadenas de texto
 
 let myName = "Brais"
-let greeting = "Hola, " + myName + "!"
-console.log(greeting)
-console.log(typeof greeting)
+let greeting = "Hola, " + myName + "!" //greeting = saludo
+console.log(greeting) // Respuesta: Hola, Brais!
+console.log(typeof greeting) // Respuesta: string
 
-// Longitud
+// Longitud. Cantidad de caracteres con espacios
 
-console.log(greeting.length)
+console.log(greeting.length) // Respuesta: 12
 
 // Acceso a caracteres
 
-console.log(greeting[0])
-console.log(greeting[11])
+console.log(greeting[0]) // Respuesta: H
+console.log(greeting[11]) // Respuesta: !
 
 // Métodos comunes
 
 console.log(greeting.toUpperCase()) // Mayúsculas
 console.log(greeting.toLowerCase()) // Minúsculas
-console.log(greeting.indexOf("Hola")) // Índice
-console.log(greeting.indexOf("Brais"))
-console.log(greeting.indexOf("MoureDev"))
-console.log(greeting.includes("Hola")) // Incluye
-console.log(greeting.includes("Brais"))
-console.log(greeting.includes("MoureDev"))
-console.log(greeting.slice(0, 10)) // Sección
-console.log(greeting.replace("Brais", "MoureDev")) // Reemplazo
+console.log(greeting.indexOf("Hola")) // Índice donde encuentra la palabra. Respuesta: 0.
+console.log(greeting.indexOf("Brais")) // Índice donde encuentra la palabra. Respuesta: 6.
+console.log(greeting.indexOf("MoureDev")) // Índice donde encuentra la palabra. Respuesta: -1 (no existe).
+console.log(greeting.includes("Hola")) // Incluye esta palabra? Respuesta: true
+console.log(greeting.includes("Brais")) // Incluye esta palabra? Respuesta: true
+console.log(greeting.includes("MoureDev")) // Incluye esta palabra? Respuesta: false
+console.log(greeting.slice(0, 10)) // Sección. Un trozo de la cadena que engloba entre el caracter 0 y el 10. Respuesta: Hola, Brai (excluye el último número, es decir, utiliza los caracteres del 0 al 9).
+console.log(greeting.replace("Brais", "MoureDev")) // Reemplazo. Respuesta: Hola, MoureDev!
 
 // Template literals (plantillas literales)
 
-// Strings en varias líneas
-let message = `Hola, este
-es mi
-curso de
-JavaScript`
-console.log(message)
+    // Strings en varias líneas. Se hace con comillas invertidas (backticks) ``.
+    let message = `Hola, este
+    es mi
+    curso de
+    JavaScript`
+    console.log(message)
 
-// Interpolación de valores
-let email = "braismoure@mouredev.com"
-console.log(`Hola, ${myName}! Tu email es ${email}.`)
+    // Interpolación de valores. Se hace con comillas invertidas (backticks) `` y ${} para insertar variables.
+    let email = "braismoure@mouredev.com"
+    console.log(`Hola, ${myName}! Tu email es ${email}.`)
--- Basic/07-strings-exercises.js
+++ Basic/07-strings-exercises.js
@@ -4,21 +4,45 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=7226
 */
 
 // 1. Concatena dos cadenas de texto
+    const firstName = "Esti";
+    const lastName = "Martin";
+
+    const fullName = firstName + " " + lastName;
+    console.log(fullName); // Respuesta: Esti Martin
 
 // 2. Muestra la longitud de una cadena de texto
+    console.log (fullName.length); // Respuesta: 11
 
 // 3. Muestra el primer y último carácter de un string
+    console.log(fullName[0]); //Respuesta: E
+    console.log(fullName[10]); //Respuesta: n
 
 // 4. Convierte a mayúsculas y minúsculas un string
+    console.log(fullName.toUpperCase()); //Respuesta: ESTI MARTIN
+    console.log(fullName.toLowerCase()); //Respuesta: esti martin
 
 // 5. Crea una cadena de texto en varias líneas
+    const multilinea = `Esto es
+    un String
+    con varias
+    líneas de texto`;
 
 // 6. Interpola el valor de una variable en un string
+    const años = 35;
+    const greeting = `Hola soy ${fullName} y tengo ${años} años.`;
+    console.log(greeting); // Respuesta: Hola soy Esti Martin y tengo 35 años
 
 // 7. Reemplaza todos los espacios en blanco de un string por guiones
+    console.log (greeting.replace(/ /g, "-")); // Respuesta: Hola-soy-Esti-Martin-y-tengo-35-años
 
 // 8. Comprueba si una cadena de texto contiene una palabra concreta
+    console.log(greeting.includes("Estibaliz")); //Respuesta: false
+    console.log(greeting.includes("Esti")); //Respuesta: true
 
 // 9. Comprueba si dos strings son iguales
+    console.log(firstName === lastName); // Respuesta: false
+    console.log(firstName === "Esti"); // Respuesta: true
 
 // 10. Comprueba si dos strings tienen la misma longitud
+    console.log(firstName.length === lastName.length); // Respuesta: false
+    console.log(firstName.length === 4); // Respuesta: true
