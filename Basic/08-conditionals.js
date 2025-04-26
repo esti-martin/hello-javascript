2f5a9af6f74fb79c418c8f52bfd8ae97e5ac98db
esti-martin <estibalizmartinborja@gmail.com>
2025-04-26 09:35:51 +0200

    08 y 09 Conditionals

--- Basic/08-conditionals.js
+++ Basic/08-conditionals.js
@@ -3,68 +3,75 @@ Clase 23 - Condicionales
 Vídeo: https://youtu.be/1glVfFxj8a4?t=7277
 */
 
+//===============================================
 // if, else if, else
+//===============================================
 
-// if (si)
+    // if (si) --> Solo se ejecuta la instrucción si se cumple la condición
 
-let age = 37
+    let age = 37
 
-if (age == 37) {
-    console.log("La edad es 37")
-}
+    if (age == 37) {
+        console.log("La edad es 37")
+    }
 
-// else (si no)
+    // else (si no) --> Se ejecutan diferentes acciones, en función de si se cumple o no la condición
 
-if (age == 37) {
-    console.log("La edad es 37")
-} else {
-    console.log("La edad no es 37")
-}
+    if (age == 37) {
+        console.log("La edad es 37")
+    } else {
+        console.log("La edad no es 37")
+    }
 
-// else if (si no, si)
+    // else if (si no, si) --> Se ejecutan diferentes acciones, en función de si se cumple o no la condición. Se pueden añadir tantas como queramos.
+    // En este caso, si la edad es 37, se ejecuta la primera instrucción. Si no, se evalúa la siguiente condición (si es menor de 18) y se ejecuta la instrucción correspondiente. Si no se cumple ninguna de las condiciones, se ejecuta la última instrucción.
 
-if (age == 37) {
-    console.log("La edad es 37")
-} else if (age < 18) {
-    console.log("Es menor de edad")
-} else {
-    console.log("La edad no es 37 ni es menor de edad")
-}
+    if (age == 37) {
+        console.log("La edad es 37")
+    } else if (age < 18) {
+        console.log("Es menor de edad")
+    } else {
+        console.log("La edad no es 37 ni es menor de edad")
+    }
 
+//===============================================
 // Operador ternario
+//===============================================
 
-const message = age == 37 ? "La edad es 37" : "La edad no es 37"
-console.log(message)
+    const message = age == 37 ? "La edad es 37" : "La edad no es 37"
+    console.log(message)
 
+//===============================================
 // switch
+//===============================================
 
-let day = 3
-let dayName
+    let day = 3
+    let dayName
 
-switch (day) {
-    case 0:
-        dayName = "Lunes"
-        break
-    case 1:
-        dayName = "Martes"
-        break
-    case 2:
-        dayName = "Miércoles"
-        break
-    case 3:
-        dayName = "Jueves"
-        break
-    case 4:
-        dayName = "Viernes"
-        break
-    case 5:
-        dayName = "Sábado"
-        break
-    case 6:
-        dayName = "Domingo"
-        break
-    default:
-        dayName = "Número de día incorrecto"
-}
+    switch (day) {
+        case 0:
+            dayName = "Lunes"
+            break
+        case 1:
+            dayName = "Martes"
+            break
+        case 2:
+            dayName = "Miércoles"
+            break
+        case 3:
+            dayName = "Jueves"
+            break
+        case 4:
+            dayName = "Viernes"
+            break
+        case 5:
+            dayName = "Sábado"
+            break
+        case 6:
+            dayName = "Domingo"
+            break
+        default:
+            dayName = "Número de día incorrecto"
+    }
 
-console.log(dayName)
+    console.log(dayName)
--- Basic/09-conditionals-exercises.js
+++ Basic/09-conditionals-exercises.js
@@ -3,27 +3,156 @@ Clase 24 - Ejercicios: Condicionales
 Vídeo: https://youtu.be/1glVfFxj8a4?t=8652
 */
 
+//===============================================
 // if/else/else if/ternaria
+//===============================================
 
 // 1. Imprime por consola tu nombre si una variable toma su valor
+    let name = "Manolo"
+
+    const myName = name == "Esti" ? "Mi nombre es Esti" : "Mi nombre no es Esti"
+    console.log(myName)
 
 // 2. Imprime por consola un mensaje si el usuario y contraseña concide con unos establecidos
+    let usuario = "esti-martin"
+    let password = "1234"
+
+    const user = (usuario == "esti-martin" && password == "1234") ? "El usuario y la contraseña son correctos" : "El usuario y/o la contraseña no son correctos"
+
+    console.log(user);
 
 // 3. Verifica si un número es positivo, negativo o cero e imprime un mensaje
+    let num = 1;
+
+    if (num < 0) {
+        console.log ("El número es negativo")
+    } else if (num > 0) {
+        console.log ("El número es positivos")
+    } else if (num == 0) {
+        console.log ("El número es cero")
+    }
 
 // 4. Verifica si una persona puede votar o no (mayor o igual a 18) e indica cuántos años le faltan
+    let edad = 15;
+
+    if (edad >= 18) {
+        console.log ("Puede votar")
+    }
+    if (edad <18) {
+        console.log ('No puede votar, le faltan ' + (18 - edad) + ' años para poder votar')
+    }
 
 // 5. Usa el operador ternario para asignar el valor "adulto" o "menor" a una variable
 //    dependiendo de la edad 
+    let edad2 = 14;
+
+    const message = edad2 >= 18 ? "Adulto" : "Menor"
+    console.log(message)
 
 // 6. Muestra en que estación del año nos encontramos dependiendo del valor de una variable "mes"
+    let mes = 'agosto';
+
+    if (mes == 'abril' || mes == 'mayo' || mes == 'junio') {
+        console.log ('Es primavera')
+    } else if (mes == 'julio' || mes == 'agosto' || mes == 'septiembre') {
+        console.log ('Es verano')
+    } else if (mes == 'octubre' || mes == 'noviembre' || mes == 'diciembre') {
+        console.log ('Es otoño')
+    } else if (mes == 'enero' || mes == 'febrero' || mes == 'marzo') {
+        console.log ('Es invierno')
+    } else {
+        console.log ('El dato introducido no es un mes')
+    }
 
 // 7. Muestra el número de días que tiene un mes dependiendo de la variable del ejercicio anterior
+    if (mes == 'enero' || 'marzo'  || 'mayo' || 'julio' || 'agosto' || 'octubre' || 'diciembre') {
+        console.log ('El mes tiene 31 días')
+    } else if (mes == 'abril' || 'junio' || 'septiembre' || 'noviembre') {
+        console.log ('El mes tiene 30 días')
+    } else if ( mes == 'febrero') {
+        console.log ('El mes tiene 28 días')
+    } else {
+        console.log ('El dato introducido no es un mes')
+    }
+    
 
+//===============================================
 // switch
+//===============================================
 
 // 8. Usa un switch para imprimir un mensaje de saludo diferente dependiendo del idioma
+    let lang = 'en';
+    let message2
+
+    switch (lang) {
+        case 'es':
+            message2 = "Hola"
+            break
+        case 'en':
+            message2 = "Hello"
+            break
+        case 'fr':
+            message2 = "Bonjour"
+            break
+        default:
+            message2 = "Idioma no soportado" 
+    }
+
+    console.log(message2);
 
 // 9. Usa un switch para hacer de nuevo el ejercicio 6
+    let estacion
+
+    switch (mes) {
+        case 'abril':
+        case 'mayo':
+        case 'junio':
+            estacion = "Primavera"
+            break
+        case 'julio':
+        case 'agosto':
+        case 'septiembre':
+            estacion = "Verano"
+            break
+        case 'octubre':
+        case 'noviembre':
+        case 'diciembre':
+            estacion = "Otoño"
+            break
+        case 'enero':
+        case 'febrero':
+        case 'marzo':
+            estacion = "Invierno"
+            break
+        default:
+            estacion = "El dato introducido no es un mes"
+    }
+    console.log(estacion);
 
 // 10. Usa un switch para hacer de nuevo el ejercicio 7
+    let message3
+
+    switch (mes) {
+        case 'enero':
+        case 'marzo':
+        case 'mayo':
+        case 'julio':
+        case 'agosto':
+        case 'octubre':
+        case 'diciembre':
+            message3 = "El mes tiene 31 días"
+            break
+        case 'abril':
+        case 'junio':
+        case 'septiembre':
+        case 'noviembre':
+            message3 = "El mes tiene 30 días"
+            break
+        case 'febrero':
+            message3 = "El mes tiene 28 días"
+            break
+        default:
+            message3 = "El dato introducido no es un mes"
+    }
+
+    console.log(message3);
