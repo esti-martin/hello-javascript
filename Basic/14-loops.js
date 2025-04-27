ecb42448f9a38cbc4d3da01df9e809ffc4847ba2
esti-martin <estibalizmartinborja@gmail.com>
2025-04-27 12:02:08 +0200

    14 y 15 Loops

--- Basic/14-loops.js
+++ Basic/14-loops.js
@@ -5,75 +5,105 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=11575
 
 // Loops o bucles
 
+// ==========================================================================
 // for
+// ==========================================================================
 
-for (let i = 0; i < 5; i++) {
-    console.log(`Hola ${i}`)
-}
+    // Se ejecuta según se lanza.
+    //Cada vez que se recorre el bucle si la condición es "true" se ejecuta el bloque de código.
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+    // i = 0 --> indica que el bucle empieza en 0
+    // i < 5 --> indica que el bucle termina cuando i sea menor que 5
+    // i++ --> indica que el bucle va a incrementar en 1 la variable i cada vez que se recorra el bucle.
 
-for (let i = 0; i < numbers.length; i++) {
-    console.log(`Elemento: ${numbers[i]}`)
-}
-
-// while
-
-let i = 0
-while (i < 5) {
-    console.log(`Hola ${i}`)
-    i++
-}
-
-// Bucle infinito
-// while(true) {
-// }
-
-// do while
-
-i = 6
-do {
-    console.log(`Hola ${i}`)
-    i++
-} while (i < 5)
-
-// for of
-
-const myArray = [1, 2, 3, 4]
-
-const mySet = new Set(["Brais", "Moure", "mouredev", 37, true, "braismoure@mouredev.com"])
-
-const myMap = new Map([
-    ["name", "Brais"],
-    ["email", "braismoure@mouredev.com"],
-    ["age", 37]
-])
-
-const myString = "¡Hola, JavaScript!"
-
-for (let value of myArray) {
-    console.log(value)
-}
-
-for (let value of mySet) {
-    console.log(value)
-}
-
-for (let value of myMap) {
-    console.log(value)
-}
-
-for (let value of myString) {
-    console.log(value)
-}
-
-// break y continue
-
-for (let i = 0; i < 10; i++) {
-    if (i == 5) {
-        continue
-    } else if (i == 7) {
-        break
+    for (let i = 0; i < 5; i++) {
+        console.log(`Hola ${i}`)
     }
-    console.log(`Hola ${i}`)
-}
+
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+    for (let i = 0; i < numbers.length; i++) {
+        console.log(`Elemento: ${numbers[i]}`) // Devuelve --> Elemento: 1  Elemento: 2  Elemento: 3  Elemento: 4  Elemento: 5  Elemento: 6  Elemento: 7  Elemento: 8
+    }
+
+// ==========================================================================
+// while
+// ==========================================================================
+
+    // Se ejecuta mientras la condición sea "true"
+    // i <5 --> Indica el periodo de tiempo que va a estar el bucle en ejecución.
+
+    let i = 0 // Crea una variable i y le asigna el valor 0
+    while (i < 5) {
+        console.log(`Hola ${i}`)
+        i++ // Incrementa la variable i en 1 cada vez que corre el bucle
+    }
+    // Devuelve --> Hola 0  Hola 1  Hola 2  Hola 3  Hola 4
+
+
+    // Bucle infinito. El código dentro de las llaves se ejecutará indefinidamente hasta el que se cierre el programa o se detenga la ejecución.
+    // while(true) {
+    // }
+
+
+// ==========================================================================
+// do while
+// ==========================================================================
+
+    // Se ejecuta al menos una vez y luego comprueba la condición.
+
+    i = 6
+    do {
+        console.log(`Hola ${i}`)
+        i++
+    } while (i < 5)
+
+    // Devuelve --> Hola 6 a pesar de no cumplir la condición y después, no se vuelve a ejecutar.
+
+
+// ==========================================================================
+// for of
+// ==========================================================================
+
+    // Se utilizar para recorer valores de algo que es iterable (estructuras de datos como arrays, strings, sets, maps, etc.)
+
+    const myArray = [1, 2, 3, 4]
+
+    const mySet = new Set(["Brais", "Moure", "mouredev", 37, true, "braismoure@mouredev.com"])
+
+    const myMap = new Map([
+        ["name", "Brais"],
+        ["email", "braismoure@mouredev.com"],
+        ["age", 37]
+    ])
+
+    const myString = "¡Hola, JavaScript!"
+
+    for (let value of myArray) {
+        console.log(value) // Devuelve --> 1 2 3 4
+    }
+
+    for (let value of mySet) {
+        console.log(value) // Devuelve --> Brais Moure mouredev 37 true braismoure@mouredev.com
+    }
+
+    for (let value of myMap) {
+        console.log(value) // Devuelve --> [ 'name', 'Brais' ] [ 'email', 'braismoure@mouredev.com' ] [ 'age', 37 ]
+    }
+
+    for (let value of myString) {
+        console.log(value) // Devuelve --> ¡ H o l a ,   J a v a S c r i p t ! (Cada letra por separado)
+    }
+
+// ==========================================================================
+// Buenaas prácticas: break y continue
+// ==========================================================================
+
+    for (let i = 0; i < 10; i++) {
+        if (i == 5) {
+            continue // Salta el número 5 y continúa con el siguiente número
+        } else if (i == 7) {
+            break // Termina el bucle cuando llega al número 7
+        }
+        console.log(`Hola ${i}`)
+    }
--- Basic/15-loops-exercises.js
+++ Basic/15-loops-exercises.js
@@ -6,21 +6,110 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=12732
 // NOTA: Explora diferentes sintaxis de bucles para resolver los ejercicios
 
 // 1. Crea un bucle que imprima los números del 1 al 20
+    for (let i1 = 1; i1 <= 20; i1++) {
+        console.log (i1)
+    }
+
 
 // 2. Crea un bucle que sume todos los números del 1 al 100 y muestre el resultado
+    let i2 = 0
+    while (i2 <= 100) {
+        console.log(i2)
+        i2++
+    }
+
 
 // 3. Crea un bucle que imprima todos los números pares entre 1 y 50
+    for (let i3 = 0; i3 <= 50; i3++) {
+        if (i3 % 2 === 0) {
+            console.log(i3)
+        }
+    }
+
 
 // 4. Dado un array de nombres, usa un bucle para imprimir cada nombre en la consola
+    const nombres = ['Juan', 'Maria', 'Pedro', 'Ana', 'Luis', 'Laura', 'Javier', 'Sofia', 'Diego', 'Valeria']
+
+    for (let i4 = 0; i4 < nombres.length; i4++) {
+        console.log (nombres[i4])
+    }
+
 
 // 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
+    const cadena = '¡Hola! ¿Cómo estás?'
+    let contador = 0
 
-// 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto
+    for (let i5 = 0; i5 < cadena.length; i5++) {
+        const letra = cadena [i5].toLowerCase()
+        if (['a', 'á', 'e', 'é', 'i', 'í', 'o', 'ó', 'u', 'ú'].includes(letra)) {
+            contador++
+        }
+    }
+    
+    console.log (contador)
+
+
+// 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto (resultado)
+    let arrayNums = [1, 2, 3, 4, 5, 6]
+    let contador2 = 1
+
+    for (let i6 = 0; i6 < arrayNums.length; i6++) {
+        const num = arrayNums [i6]
+        contador2 *= num
+    }
+
+    console.log (contador2)
+
 
 // 7. Escribe un bucle que imprima la tabla de multiplicar del 5
+    for (let i7 = 5; i7 <= 50; i7 += 5) {
+        console.log (i7)
+
+    }
+
 
 // 8. Usa un bucle para invertir una cadena de texto
+    let cadena2 = "¡Hola, JavaScript!"
+    let cadenaInvertida = ""
+
+    for (let i8 = cadena2.length -1; i8 >= 0; i8--) {
+        cadenaInvertida += cadena2[i8]
+    }
+
+    console.log (cadenaInvertida)
+
 
 // 9. Usa un bucle para generar los primeros 10 números de la secuencia de Fibonacci
+    //Primeros números de la secuencia de Fibonacci
+    let fib1 = 0;
+    let fib2 = 1;
+    //Array para almacenar los números iniciacizado con el primer número (fib1 = 0)
+    let fibonacci = [fib1];
+
+    //El bucle se ejecuta 10 veces para generar los primeros 10 números de la secuencia
+    for (let i9 = 0; i9 < 10; i9++) {
+        //Se declara fib3 como el siguiente número de la secuencia de Fibonacci
+        let fib3 = fib1 + fib2
+
+        //Se actualizan los valores
+        fib1 = fib2
+        fib2 = fib3
+
+        //Se añade el número al array
+        fibonacci.push(fib1)
+    }
+
+    console.log(fibonacci)
 
 // 10. Dado un array de números, usa un bucle para crear un nuevo array que contenga solo los números mayores a 10
+    let myNums = [8, 2, 541, 54, 1, 12, 84, 4, 465]
+
+    for (let i10 = 0; i10 < myNums.length; i10++) {
+        if (myNums[i10] >= 10) {
+            console.log (myNums[i10])
+        }
+    }
+
+
+
+    
